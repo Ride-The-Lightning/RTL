530ca2becb0cc42eb9ca33cce4d22c27bb824de1
ShahanaFarooqui <shahana.farooqui@gmail.com>
2018-12-11 19:06:57 -0500

    ForEach fix with Issue #1
    ForEach fix with Issue #1

--- controllers/payments.js
+++ controllers/payments.js
@@ -15,9 +15,11 @@ exports.getPayments = (req, res, next) => {
         error: (undefined === body || search_idx > -1) ? 'Error From Server!' : body.error
       });
     } else {
-      body.payments.forEach(payment => {
-        payment.creation_date_str =  (undefined === payment.creation_date) ? '' : common.convertTimestampToDate(payment.creation_date);
-      });
+      if (undefined !== body.payments) {
+        body.payments.forEach(payment => {
+          payment.creation_date_str =  (undefined === payment.creation_date) ? '' : common.convertTimestampToDate(payment.creation_date);
+        });
+      }
       res.status(200).json(body.payments);
     }
   });
