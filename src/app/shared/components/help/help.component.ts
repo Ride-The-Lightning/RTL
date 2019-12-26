2965d707fec47d663981e141e645e74b4e8b9255
saubyk <39208279+saubyk@users.noreply.github.com>
2019-12-25 23:08:12 -0500

    help documentation

--- src/app/shared/components/help/help.component.ts
+++ src/app/shared/components/help/help.component.ts
@@ -23,8 +23,23 @@ export class HelpComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    this.helpTopics.push(new HelpTopic('Change setting?', 'Click on setting icon on the navigation and choose from the given options.'));
-    // this.helpTopics.push(new HelpTopic('', ''));
+    this.helpTopics.push(new HelpTopic('Getting started', 'Funding you node is the first step to start ' +
+    'operating on Lightning Network. To fund your node go to the *On-chain* menu of the application.' +
+    ' Click on the *Receive* section of *On-Chain Transactions*, generate a new address' +
+     ' and send funds on that address. Once the node is funded, next step is to connect with network peers ' +
+      'and open channels with them.'));
+    this.helpTopics.push(new HelpTopic('Connect with peers', 'To connect with peers go the *Peer/Channels* ' +
+    'menu under the *Lightning* menu. To connect with a peer you need a pubkey and host information in the ' +
+    'pubkey@ip:port format. You can connect with a peer on the  *Peers* section or choose the *ADD PEER* ' +
+    'option from the Alias drop-down on *Channels* section. Once the peer is connected, you can open ' +
+    'channels with them'));
+    this.helpTopics.push(new HelpTopic('Opening Channels', 'To open channel with a peers go the *Peer/Channels* ' +
+    'menu under the *Lightning* menu. On the *Channels* section, select the Alias of the connected peer from ' +
+    'the drop-down, specify the amount to commit to the channel. There are a variety of options which can be ' +
+    'specified while opening a channel. \n' +
+    '1. Private Channel - When this option is selected, the channel is opened privately and not broadcast. \n' +
+    '2. Priority (advanced option) - Specify either Target confirmation Block or Fee in Sat/Byte. \n' +
+    '3. Spend Unconfirmd Output (advanced option) - Allow channels to be opened with unconfirmed UTXOs.'));
   }
 
 }
