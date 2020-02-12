fa243ad257cb8300506e3009df228a85ec05ff5a
Shahana Farooqui <shahana.farooqui@gmail.com>
2020-02-12 17:59:51 -0500

    Repeated forwarding history calls Issue #218
    Repeated forwarding history calls Issue #218

--- controllers/lnd/switch.js
+++ controllers/lnd/switch.js
@@ -1,138 +1,59 @@
 var request = require('request-promise');
+var request = require('request-promise');
 var common = require('../../common');
 var logger = require('../logger');
 var options = {};
-
-exports.getAllForwardingEvents = (start, end, offset, max_events) => {
-  return new Promise(function(resolve, reject) {
-    options = common.getOptions();
-    options.url = common.getSelLNServerUrl() + '/switch';
-    options.form = {};
-    if (start) { options.form.start_time = start; }
-    if (end) { options.form.end_time = end; }
-    options.form.num_max_events = max_events ? max_events : 1000;
-    options.form.index_offset = offset ? offset : 0;
-    options.form = JSON.stringify(options.form);
-    logger.info({fileName: 'Switch', msg: 'Forwarding History Params: ' + options.form});
-    request.post(options).then((body) => {
-      if(undefined === body || body.error) {
-        logger.error({fileName: 'Switch', lineNum: 31, msg: 'Forwarding History Error: ' + JSON.stringify((undefined === body) ? 'Error From Server!' : body.error)});
-        res.status(500).json({
-          message: "Switch post failed!",
-          error: (undefined === body) ? 'Error From Server!' : body.error
-        });
-      } else {
-        logger.info({fileName: 'Switch', msg: 'Forwarding History Received: ' + JSON.stringify(body)});
-        if (body.forwarding_events && body.forwarding_events.length > 0) {
-          body.forwarding_events.forEach(event => {
-            event.timestamp_str =  (undefined === event.timestamp) ? '' : common.convertTimestampToDate(event.timestamp);
-          });
-          body.forwarding_events = common.sortDescByKey(body.forwarding_events, 'timestamp');
-        }
-        logger.info({fileName: 'Switch', msg: 'Forwarding History before Resolve: ' + JSON.stringify(body)});
-        resolve(body);
-      }
-    })
-    .catch(function (err) {
-      logger.error({fileName: 'Switch', lineNum: 48, msg: 'Forwarding History Error: ' + JSON.stringify(err)});
-      resolve({
-        message: "Switch post failed!",
-        error: err.error
-      });
-    });
-  });
-}
+var num_max_events = 100;
+var responseData = { forwarding_events: [], last_offset_index: 0 };
 
 exports.forwardingHistory = (req, res, next) => {
-  this.getAllForwardingEvents(req.body.start_time, req.body.end_time, 0, 1000).then(function(values) {
-    logger.info({fileName: 'Switch', msg: 'All Forwarding Events: ' + JSON.stringify(values)});
-    res.status(201).json(values);
+  responseData = { forwarding_events: [], last_offset_index: 0 };
+  this.getAllForwardingEvents(req.body.start_time, req.body.end_time, 0, (eventsResponse) => {
+    if (eventsResponse.error) {
+      res.status(500).json(eventsResponse);
+    } else {
+      if (eventsResponse.forwarding_events && eventsResponse.forwarding_events.length > 0) {
+        eventsResponse.forwarding_events.forEach(event => {
+          event.timestamp_str =  !event.timestamp ? '' : common.convertTimestampToDate(event.timestamp);
+        });
+        eventsResponse.forwarding_events = common.sortDescByKey(eventsResponse.forwarding_events, 'timestamp');
+      }
+      res.status(201).json(eventsResponse);
+    }
+  });
+};
+
+exports.getAllForwardingEvents = (start, end, offset, callback) => {
+  options = common.getOptions();
+  options.url = common.getSelLNServerUrl() + '/switch';
+  options.form = {};
+  if (start) { options.form.start_time = start; }
+  if (end) { options.form.end_time = end; }
+  options.form.num_max_events = num_max_events;
+  options.form.index_offset = offset;
+  options.form = JSON.stringify(options.form);
+  logger.info({fileName: 'Switch', msg: 'Forwarding History Params: ' + options.form});
+  request.post(options).then((body) => { 
+    logger.info({fileName: 'Switch', msg: 'Forwarding History: ' + JSON.stringify(body)});
+    try {
+      if (body.forwarding_events) { 
+        console.warn('LENGTH: ' + body.forwarding_events.length);
+        responseData.forwarding_events.push(...body.forwarding_events);
+      }
+      if (!body.last_offset_index || body.last_offset_index < offset + num_max_events) {
+        responseData.last_offset_index = body.last_offset_index ? body.last_offset_index : 0
+        callback(responseData);
+      } else {
+        this.getAllForwardingEvents(start, end, offset + num_max_events, callback);
+      }    
+    } catch(err) {
+      console.warn(err);
+    }
   }).catch(err => {
-    logger.error({fileName: 'Switch', lineNum: 60, msg: 'Get All Forwarding Events: ' + JSON.stringify(err)});
-    res.status(500).json({
-      message: "Switch post failed!",
+    logger.error({fileName: 'Switch', lineNum: 32, msg: 'Get All Forwarding Events Failed: ' + JSON.stringify(err)});
+    callback({
+      message: "Forwarding Events Failed!",
       error: err.error
     });
   });  
-};
-
-
-
-
-
-
-
-
-// var request = require('request-promise');
-// var common = require('../../common');
-// var logger = require('../logger');
-// var options = {};
-// var num_max_events = 10;
-
-// exports.fetchPaginatedEvents = (start, end, offset, results2, callback) => {
-//   options = common.getOptions();
-//   options.url = common.getSelLNServerUrl() + '/switch';
-//   options.form = {};
-//   if (start) { options.form.start_time = start; }
-//   if (end) { options.form.end_time = end; }
-//   options.form.num_max_events = num_max_events;
-//   options.form.index_offset = offset ? offset : 0;
-//   options.form = JSON.stringify(options.form);
-//   logger.info({fileName: 'Switch', msg: 'Forwarding History Params: ' + options.form});
-//   return request.post(options).then((body) => {
-//     logger.info({fileName: 'Switch', msg: 'Forwarding History Received: ' + JSON.stringify(body)});
-//     if(undefined === body || body.error) {
-//       logger.error({fileName: 'Switch', lineNum: 31, msg: 'Forwarding History Error: ' + JSON.stringify((undefined === body) ? 'Error From Server!' : body.error)});
-//       res.status(500).json({
-//         message: "Switch post failed!",
-//         error: (undefined === body) ? 'Error From Server!' : body.error
-//       });
-//     } else {
-//       if (undefined !== body.forwarding_events && body.forwarding_events.length > 0) {
-//         body.forwarding_events.forEach(event => {
-//           event.timestamp_str =  (undefined === event.timestamp) ? '' : common.convertTimestampToDate(event.timestamp);
-//         });
-//         body.forwarding_events = common.sortDescByKey(body.forwarding_events, 'timestamp');
-//       }
-//       logger.info({fileName: 'Switch', msg: 'Forwarding History Before Resolve: ' + JSON.stringify(body)});
-//       if (body.last_offset_index >= (offset+num_max_events)) {
-//         // console.log('INSIDE CALLBACK');
-//         // console.log(results);
-//         // results.push(body)
-//         callback(results2);
-//       }
-//       console.log('INSIDE RESOLVE');
-//       // console.log(body);
-//       console.log(results2);
-//       body.forwarding_events.forEach(event => {
-//         results2.push(results2);
-//       });
-//       console.log(results2);
-//       return results2;
-//     }
-//   }).catch(err => err);
-// }
-
-// exports.getAllForwardingEvents = (start, end, offset, results1) => {
-//   var self = this;
-//   return new Promise((resolve, reject) => {
-//     resolve(self.fetchPaginatedEvents(start, end, offset, results1, function(results1) { self.getAllForwardingEvents(start, end, offset + num_max_events, results1)}));
-//   });
-// }
-
-// exports.forwardingHistory = (req, res, next) => {
-//   var results = [];
-//   this.getAllForwardingEvents(req.body.start_time, req.body.end_time, 0, results)
-//   .then(values => {
-//     console.log('VALUES: ');
-//     console.log(values);
-//     logger.info({fileName: 'Switch', msg: 'All Forwarding Events: ' + JSON.stringify(values)});
-//     res.status(201).json(values);
-//   }).catch(err => {
-//     logger.error({fileName: 'Switch', lineNum: 60, msg: 'Get All Forwarding Events: ' + JSON.stringify(err)});
-//     res.status(500).json({
-//       message: "Switch post failed!",
-//       error: err.error
-//     });
-//   });  
-// };
+}
