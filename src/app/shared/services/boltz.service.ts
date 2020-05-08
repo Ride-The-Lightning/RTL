7c0dd5abbb658cf3419c646c1ede2322d50b160d
Dejan Kocev <dejan_kocev@hotmail.com>
2020-05-09 00:55:27 +0200

    remove unused imports

--- src/app/shared/services/boltz.service.ts
+++ src/app/shared/services/boltz.service.ts
@@ -1,7 +1,7 @@
-import { Injectable, Output } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { throwError, of, from } from 'rxjs';
-import { catchError, mergeMap, takeUntil, filter } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { catchError, mergeMap } from 'rxjs/operators';
 import { Store } from '@ngrx/store';
 import { ECPair, crypto, Transaction, address } from 'bitcoinjs-lib';
 import { detectSwap, constructClaimTransaction, Networks } from 'boltz-core';
