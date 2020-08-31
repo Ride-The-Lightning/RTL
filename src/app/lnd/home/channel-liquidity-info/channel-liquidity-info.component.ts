0642b567a444f8e64fcaf76dc34e6f54185379bb
Dejan Kocev <dejan_kocev@hotmail.com>
2020-08-31 17:48:04 +0200

    updated channel swap menu

--- src/app/lnd/home/channel-liquidity-info/channel-liquidity-info.component.ts
+++ src/app/lnd/home/channel-liquidity-info/channel-liquidity-info.component.ts
@@ -7,7 +7,10 @@ import { Store } from '@ngrx/store';
 import { SwapTypeEnum } from '../../../shared/services/consts-enums-functions';
 import { Channel } from '../../../shared/models/lndModels';
 import { LoopModalComponent } from '../../loop/loop-modal/loop-modal.component';
+import { BoltzModalComponent } from '../../boltz/boltz-modal/boltz-modal.component';
 import { LoopService } from '../../../shared/services/loop.service';
+import { BoltzService } from '../../../shared/services/boltz.service';
+
 
 import * as fromRTLReducer from '../../../store/rtl.reducers';
 import * as RTLActions from '../../../store/rtl.actions';
@@ -25,7 +28,7 @@ export class ChannelLiquidityInfoComponent implements OnInit, OnDestroy {
   private targetConf = 6;
   private unSubs: Array<Subject<void>> = [new Subject(), new Subject()];
 
-  constructor(private router: Router, private loopService: LoopService, private store: Store<fromRTLReducer.RTLState>) {}
+  constructor(private router: Router, private loopService: LoopService, private boltzService: BoltzService, private store: Store<fromRTLReducer.RTLState>) {}
 
   ngOnInit() {
     this.store.select('lnd')
@@ -40,11 +43,49 @@ export class ChannelLiquidityInfoComponent implements OnInit, OnDestroy {
   }
 
   onLoopOut(channel: Channel) {
-    this.store.dispatch(new RTLActions.OpenAlert({ data: {
-      channel: channel,
-      direction: SwapTypeEnum.WITHDRAWAL,
-      component: LoopModalComponent
-    }}));
+    this.store.dispatch(new RTLActions.OpenSpinner('Getting Terms and Quotes...'));
+    this.loopService.getLoopOutTermsAndQuotes(this.targetConf)
+    .pipe(takeUntil(this.unSubs[1]))
+    .subscribe(response => {
+      this.store.dispatch(new RTLActions.CloseSpinner());
+      this.store.dispatch(new RTLActions.OpenAlert({ minHeight: '56rem', data: {
+        channel: channel,
+        minQuote: response[0],
+        maxQuote: response[1],
+        direction: SwapTypeEnum.LOOP_OUT,
+        component: LoopModalComponent
+      }}));    
+    });
+  }  
+
+  onSubmarine(channel: Channel) {
+    this.store.dispatch(new RTLActions.OpenSpinner('Getting Pairs...'));
+    this.boltzService.getPairs()
+    .pipe(takeUntil(this.unSubs[0]))
+    .subscribe(response => {
+      const pairs = response['pairs']["BTC/BTC"];
+      const lndNode = this.boltzService.getLNDNode();
+      this.store.dispatch(new RTLActions.CloseSpinner());
+      const minQuote = {
+        swap_fee_sat: (pairs.limits.minimal * pairs.fees.percentage * 0.01).toString(),
+        htlc_publish_fee_sat: pairs.fees.minerFees.baseAsset.normal.toString(),
+        swap_payment_dest: lndNode,
+        amount: pairs.limits.minimal
+      };
+      const maxQuote = {
+        swap_fee_sat: (pairs.limits.maximal * pairs.fees.percentage * 0.01).toString(),
+        htlc_publish_fee_sat: pairs.fees.minerFees.baseAsset.normal.toString(),
+        swap_payment_dest: lndNode,
+        amount: pairs.limits.maximal
+      };
+      this.store.dispatch(new RTLActions.OpenAlert({ data: {
+        channel,
+        minQuote,
+        maxQuote,
+        direction: SwapTypeEnum.WITHDRAWAL,
+        component: BoltzModalComponent
+      }}));
+    })
   }  
 
   ngOnDestroy() {
