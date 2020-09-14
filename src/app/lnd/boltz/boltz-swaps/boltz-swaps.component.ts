b9181869ffc319460a13a930b6030fb54389e4ec
Dejan Kocev <dejan_kocev@hotmail.com>
2020-09-14 22:11:14 +0200

    fix table date time sort

--- src/app/lnd/boltz/boltz-swaps/boltz-swaps.component.ts
+++ src/app/lnd/boltz/boltz-swaps/boltz-swaps.component.ts
@@ -9,7 +9,7 @@ import { MatPaginator, MatPaginatorIntl } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { SwapStatus } from '../../../shared/models/lndModels';
-import { PAGE_SIZE, PAGE_SIZE_OPTIONS, getPaginatorLabel, AlertTypeEnum, DataTypeEnum, ScreenSizeEnum, SwapTypeEnum, SwapStateEnum, SwapProviderEnum } from '../../../shared/services/consts-enums-functions';
+import { PAGE_SIZE, PAGE_SIZE_OPTIONS, getPaginatorLabel, AlertTypeEnum, DataTypeEnum, ScreenSizeEnum, SwapTypeEnum, SwapStateEnum } from '../../../shared/services/consts-enums-functions';
 import { LoggerService } from '../../../shared/services/logger.service';
 import { CommonService } from '../../../shared/services/common.service';
 import { LoopService } from '../../../shared/services/loop.service';
@@ -28,8 +28,8 @@ import * as LNDActions from '../../store/lnd.actions';
 })
 export class BoltzSwapsComponent implements OnInit, OnChanges, OnDestroy {
   @Input() selectedSwapType: SwapTypeEnum = SwapTypeEnum.WITHDRAWAL;
-  @ViewChild(MatSort, { static: true }) sort: MatSort;
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
+  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
   public swapStateEnum = SwapStateEnum;
   public faHistory = faHistory;
   public swapCaption = 'Withdrawal';
@@ -76,8 +76,9 @@ export class BoltzSwapsComponent implements OnInit, OnChanges, OnDestroy {
       if (rtlStore.boltzSwaps) {
         this.storedSwaps = rtlStore.boltzSwaps.map(swap => ({...swap, state: Object.keys(SwapStateEnum).find(ss => SwapStateEnum[ss] === swap.state)}));
         this.filteredSwaps = this.storedSwaps.filter(swap => swap.type === this.selectedSwapType).sort((a, b) => {
-          const x = new Date(a.initiation_time_str);
-          const y = new Date(b.initiation_time_str);
+
+          const x = a.initiation_time_str;
+          const y = b.initiation_time_str;
           return x>y ? -1 : x<y ? 1 : 0;
         });
         this.loadSwapsTable(this.filteredSwaps);
@@ -100,52 +101,39 @@ export class BoltzSwapsComponent implements OnInit, OnChanges, OnDestroy {
   }
 
   onSwapClick(selSwap: SwapStatus, event: any) {
-    if(selSwap.provider === SwapProviderEnum.BOLTZ) {
-      const reorderedSwap = [
-        [{key: 'state', value: SwapStateEnum[selSwap.state], title: 'Status', width: 50, type: DataTypeEnum.STRING},
-          {key: 'amt', value: selSwap.amt, title: 'Amount (Sats)', width: 50, type: DataTypeEnum.NUMBER}],
-        [{key: 'initiation_time_str', value: selSwap.initiation_time_str, title: 'Initiation Time', width: 50, type: DataTypeEnum.DATE_TIME},
-          {key: 'last_update_time_str', value: selSwap.last_update_time_str, title: 'Last Update Time', width: 50, type: DataTypeEnum.DATE_TIME}],
-        [{key: 'cost_server', value: selSwap.cost_server, title: 'Server Cost (Sats)', width: 33, type: DataTypeEnum.NUMBER},
-          {key: 'cost_offchain', value: selSwap.cost_offchain, title: 'Offchain Cost (Sats)', width: 33, type: DataTypeEnum.NUMBER},
-          {key: 'cost_onchain', value: selSwap.cost_onchain, title: 'Onchain Cost (Sats)', width: 34, type: DataTypeEnum.NUMBER}],
-        [{key: 'id_bytes', value: selSwap.id_bytes, title: 'ID', width: 100, type: DataTypeEnum.STRING}],
-        [{key: 'htlc_address', value: selSwap.htlc_address, title: 'HTLC Address', width: 100, type: DataTypeEnum.STRING}]
-      ];
-      this.store.dispatch(new RTLActions.OpenAlert({ data: {
-        type: AlertTypeEnum.INFORMATION,
-        alertTitle: this.swapCaption + ' Status',
-        message: reorderedSwap,
-        openedBy: 'SWAP'
-      }}));
-    } else {
-      this.loopService.getSwap(selSwap.id_bytes.replace(/\//g, '_').replace(/\+/g, '-')).pipe(takeUntil(this.unSubs[2]))
-      .subscribe((fetchedSwap: SwapStatus) => {
-        const reorderedSwap = [
-          [{key: 'state', value: SwapStateEnum[fetchedSwap.state], title: 'Status', width: 50, type: DataTypeEnum.STRING},
-            {key: 'amt', value: fetchedSwap.amt, title: 'Amount (Sats)', width: 50, type: DataTypeEnum.NUMBER}],
-          [{key: 'initiation_time_str', value: fetchedSwap.initiation_time_str, title: 'Initiation Time', width: 50, type: DataTypeEnum.DATE_TIME},
-            {key: 'last_update_time_str', value: fetchedSwap.last_update_time_str, title: 'Last Update Time', width: 50, type: DataTypeEnum.DATE_TIME}],
-          [{key: 'cost_server', value: fetchedSwap.cost_server, title: 'Server Cost (Sats)', width: 33, type: DataTypeEnum.NUMBER},
-            {key: 'cost_offchain', value: fetchedSwap.cost_offchain, title: 'Offchain Cost (Sats)', width: 33, type: DataTypeEnum.NUMBER},
-            {key: 'cost_onchain', value: fetchedSwap.cost_onchain, title: 'Onchain Cost (Sats)', width: 34, type: DataTypeEnum.NUMBER}],
-          [{key: 'id_bytes', value: fetchedSwap.id_bytes, title: 'ID', width: 100, type: DataTypeEnum.STRING}],
-          [{key: 'htlc_address', value: fetchedSwap.htlc_address, title: 'HTLC Address', width: 100, type: DataTypeEnum.STRING}]
-        ];
-        this.store.dispatch(new RTLActions.OpenAlert({ data: {
-          type: AlertTypeEnum.INFORMATION,
-          alertTitle: this.swapCaption + ' Status',
-          message: reorderedSwap,
-          openedBy: 'SWAP'
-        }}));
-      });
-    }
+    const reorderedSwap = [
+      [{key: 'state', value: SwapStateEnum[selSwap.state], title: 'Status', width: 50, type: DataTypeEnum.STRING},
+        {key: 'amt', value: selSwap.amt, title: 'Amount (Sats)', width: 50, type: DataTypeEnum.NUMBER}],
+      [{key: 'initiation_time_str', value: selSwap.initiation_time_str, title: 'Initiation Time', width: 50, type: DataTypeEnum.NUMBER},
+        {key: 'last_update_time_str', value: selSwap.last_update_time_str, title: 'Last Update Time', width: 50, type: DataTypeEnum.NUMBER}],
+      [{key: 'cost_server', value: selSwap.cost_server, title: 'Server Cost (Sats)', width: 33, type: DataTypeEnum.NUMBER},
+        {key: 'cost_offchain', value: selSwap.cost_offchain, title: 'Offchain Cost (Sats)', width: 33, type: DataTypeEnum.NUMBER},
+        {key: 'cost_onchain', value: selSwap.cost_onchain, title: 'Onchain Cost (Sats)', width: 34, type: DataTypeEnum.NUMBER}],
+      [{key: 'id_bytes', value: selSwap.id_bytes, title: 'ID', width: 100, type: DataTypeEnum.STRING}],
+      [{key: 'htlc_address', value: selSwap.htlc_address, title: 'HTLC Address', width: 100, type: DataTypeEnum.STRING}]
+    ];
+    this.store.dispatch(new RTLActions.OpenAlert({ data: {
+      type: AlertTypeEnum.INFORMATION,
+      alertTitle: this.swapCaption + ' Status',
+      message: reorderedSwap,
+      openedBy: 'SWAP'
+    }}));
   }
 
   loadSwapsTable(swaps) {
     this.listSwaps = new MatTableDataSource<SwapStatus>([...swaps]);
     this.listSwaps.sort = this.sort;
-    this.listSwaps.sortingDataAccessor = (data, sortHeaderId) => (data[sortHeaderId]  && isNaN(data[sortHeaderId])) ? data[sortHeaderId].toLocaleLowerCase() : +data[sortHeaderId];
+    this.listSwaps.sortingDataAccessor = (data, sortHeaderId) => {
+      switch(sortHeaderId) {
+        case 'initiation_time':
+          return Date.parse(data.initiation_time_str);
+        case 'last_update_time':
+          return Date.parse(data.last_update_time_str);
+        default:
+        return  (data[sortHeaderId]  && isNaN(data[sortHeaderId])) ? data[sortHeaderId].toLocaleLowerCase() : +data[sortHeaderId];
+
+      }
+    }
     this.listSwaps.paginator = this.paginator;
     this.logger.info(this.listSwaps);
   }
