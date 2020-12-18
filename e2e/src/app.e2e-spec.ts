bc36694a0e3dc7a4e47540bb2c1666c225d02680
Shahana Farooqui <shahana.farooqui@gmail.com>
2020-12-18 18:18:57 -0500

    NG11 testing complete
    NG11 testing complete

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('project-upgrade app is running!');
+    expect(await page.getTitleText()).toEqual('RTL app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/project-upgrade'),
+      dir: require('path').join(__dirname, './coverage/RTL'),
       subdir: '.',
       reporters: [
         { type: 'html' },
