2c7d62e256a712b3b71af3820f1eb7b39363c83b
violet360 <mazedk2k@gmail.com>
2021-08-24 17:22:36 +0530

    bumpfee along with err detection

--- src/app/clightning/peers-channels/channels/bump-fee-modal/bump-fee.component.ts
+++ src/app/clightning/peers-channels/channels/bump-fee-modal/bump-fee.component.ts
@@ -3,12 +3,13 @@ import { NgModel } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Subject } from 'rxjs';
-import { take } from 'rxjs/operators';
+import { filter, take, takeUntil } from 'rxjs/operators';
+import { Actions } from '@ngrx/effects';
 import { faCopy, faInfoCircle, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 import * as fromRTLReducer from '../../../../store/rtl.reducers';
 import { Channel } from '../../../../shared/models/clModels';
 import { CLChannelInformation } from '../../../../shared/models/alertData';
-import { ADDRESS_TYPES, TRANS_TYPES } from '../../../../shared/services/consts-enums-functions';
+import { ADDRESS_TYPES, APICallStatusEnum, TRANS_TYPES } from '../../../../shared/services/consts-enums-functions';
 import { DataService } from '../../../../shared/services/data.service';
 import { LoggerService } from '../../../../shared/services/logger.service';
 import { Store } from '@ngrx/store';
@@ -47,41 +48,40 @@ export class CLBumpFeeComponent implements OnInit, OnDestroy {
   private unSubs: Array<Subject<void>> = [new Subject(), new Subject()];
 
 
-  constructor(public dialogRef: MatDialogRef<CLBumpFeeComponent>, @Inject(MAT_DIALOG_DATA) public data: CLChannelInformation, private store: Store<fromRTLReducer.RTLState>, private clEffects: CLEffects, private logger: LoggerService, private dataService: DataService, private snackBar: MatSnackBar) {}
+  constructor(private actions: Actions, public dialogRef: MatDialogRef<CLBumpFeeComponent>, @Inject(MAT_DIALOG_DATA) public data: CLChannelInformation, private store: Store<fromRTLReducer.RTLState>, private clEffects: CLEffects, private logger: LoggerService, private dataService: DataService, private snackBar: MatSnackBar) { }
 
   ngOnInit() {
     this.bumpFeeChannel = this.data.channel;
   }
 
-  onBumpFee(): boolean|void {
-    if (!this.outputIndex || !this.fees) {
-      console.warn(typeof this.outputIndex.toString(), '---', typeof this.fees);
+  onBumpFee(): boolean | void {
+    if (this.outputIndex  === null|| this.fees === null) {
       return true;
     }
     const utxoString = this.bumpFeeChannel.funding_txid + ':' + this.outputIndex.toString();
     this.store.dispatch(new CLActions.GetNewAddress(this.selectedAddressType));
-    this.clEffects.setNewAddressCL.
-      pipe(take(1)).
-      subscribe({ next : (newAddress) => {
-        this.newAddress = newAddress;
-        console.warn(this.newAddress, ', all ,', this.fees, 0, [utxoString], '\n');
-        this.store.dispatch(new CLActions.SetChannelTransaction({
-          address:this.newAddress,
-          satoshis:'all',
-          feeRate:this.fees,
-          utxos:[utxoString]
-        }));
-        this.clEffects.SetChannelTransactionCL.
-          pipe(take(1)).
-          subscribe({ next:(data) => {
-            this.dialogRef.close(false);
-          }, error:(err) => {
-            this.logger.error(err);
-          } });
-      }, error: (err) => {
-        this.bumpFeeError = err.message ? err.message : err;
-        this.logger.error(err);
-      } });
+    this.actions.pipe(
+      takeUntil(this.unSubs[1]),
+      filter((action) => action.type === CLActions.SET_NEW_ADDRESS_CL || action.type === CLActions.SET_CHANNEL_TRANSACTION_RES_CL || action.type === CLActions.UPDATE_API_CALL_STATUS_CL)).
+      subscribe((action: (CLActions.SetNewAddress | CLActions.SetChannelTransactionRes | CLActions.UpdateAPICallStatus)) => {
+        if (action.type === CLActions.SET_NEW_ADDRESS_CL) {
+          console.log(action.payload, 'all', this.fees, utxoString)
+          this.store.dispatch(new CLActions.SetChannelTransaction({
+            address: action.payload,
+            satoshis: 'all',
+            feeRate: this.fees,
+            utxos: [utxoString]
+          }));
+        }
+        if (action.type === CLActions.SET_CHANNEL_TRANSACTION_RES_CL) {
+          this.dialogRef.close()
+        }
+        if (action.type === CLActions.UPDATE_API_CALL_STATUS_CL && action.payload.status === APICallStatusEnum.ERROR) {
+          if (action.payload.action === 'SetChannelTransaction' || action.payload.action === 'GenerateNewAddress') {
+            this.bumpFeeError = action.payload.message;
+          }
+        }
+      });
   }
 
   onCopyID(payload: string) {
