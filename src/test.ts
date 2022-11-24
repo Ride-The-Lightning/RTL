be0d1775d013859817d17b022e09e4c08dec6f0e
ShahanaFarooqui <shahana.farooqui@gmail.com>
2022-11-23 19:30:45 -0800

    ng v15 update

--- src/test.ts
+++ src/test.ts
@@ -2,15 +2,7 @@
 import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
-
-declare const require: any;
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting(), { teardown: { destroyAfterEach: false } }
 );
-
-const context = require.context('./', true, /\.spec\.ts$/);
-// const context = require.context('./app/lnd/transactions/send-payment-modal', true, /\.spec\.ts$/);
-// const context = require.context('./app/store', true, /\.spec\.ts$/);
-
-context.keys()?.map(context);
