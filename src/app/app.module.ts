d8478308369bce69ba08e47c677a66cc4bb90ba7
ShahanaFarooqui <shahana.farooqui@gmail.com>
2023-03-01 13:43:16 -0800

    Router lint bug fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -42,7 +42,7 @@ if (isDevMode()) { isDevEnvironemt = true; }
     routing,
     LayoutModule,
     HammerModule,
-    UserIdleModule.forRoot({ idle: (HOUR_SECONDS-10), timeout: 10, ping: 12000 }),
+    UserIdleModule.forRoot({ idle: (HOUR_SECONDS - 10), timeout: 10, ping: 12000 }),
     StoreModule.forRoot(
       { root: RootReducer, lnd: LNDReducer, cln: CLNReducer, ecl: ECLReducer },
       {
